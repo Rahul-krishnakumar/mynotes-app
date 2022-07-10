c9b0f964730256c0a2841f532b5aad71c5675c3d
Rahul <rahul967@hotmail.com>
2022-07-10 12:39:56 +0530

    Seperated register and login views, and added router navigation between them

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mynotes/firebase_options.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -31,56 +29,50 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: FutureBuilder(
-            future: Firebase.initializeApp(
-                options: DefaultFirebaseOptions.currentPlatform),
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.done:
-                  return Column(
-                    children: [
-                      TextField(
-                        autocorrect: false,
-                        controller: _email,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(hintText: 'Email'),
-                      ),
-                      TextField(
-                        autocorrect: false,
-                        enableSuggestions: false,
-                        controller: _password,
-                        decoration: const InputDecoration(hintText: 'Password'),
-                        obscureText: true,
-                      ),
-                      TextButton(
-                          onPressed: () async {
-                            final email = _email.text;
-                            final password = _password.text;
-                            try {
-                              final userCredentials = await FirebaseAuth
-                                  .instance
-                                  .signInWithEmailAndPassword(
-                                      email: email, password: password);
-                              print(userCredentials);
-                            } on FirebaseAuthException catch (e) {
-                              if (e.code == 'user-not-found') {
-                                print('User not found!');
-                              } else if (e.code == 'wrong-password') {
-                                print('Wrong password!');
-                              } else {
-                                print('Oops, something went wrong!');
-                              }
-                            }
-                          },
-                          child: const Text('Login')),
-                    ],
-                  );
-                default:
-                  return const Text('Loading...');
-              }
-            }));
+      appBar: AppBar(title: const Text('Login')),
+      body: Column(
+        children: [
+          TextField(
+            autocorrect: false,
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: 'Email'),
+          ),
+          TextField(
+            autocorrect: false,
+            enableSuggestions: false,
+            controller: _password,
+            decoration: const InputDecoration(hintText: 'Password'),
+            obscureText: true,
+          ),
+          TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                try {
+                  final userCredentials = await FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: email, password: password);
+                  print(userCredentials);
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'user-not-found') {
+                    print('User not found!');
+                  } else if (e.code == 'wrong-password') {
+                    print('Wrong password!');
+                  } else {
+                    print('Oops, something went wrong!');
+                  }
+                }
+              },
+              child: const Text('Login')),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/register', (route) => false);
+              },
+              child: const Text('Don\'t have an account? Register here!'))
+        ],
+      ),
+    );
   }
 }
